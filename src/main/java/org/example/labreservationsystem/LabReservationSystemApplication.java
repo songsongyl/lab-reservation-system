a0cc7e9c2682c44ea885001f571e66f50377eda4
songsongyl <2444616264@qq.com>
2024-12-12 15:28:17 +0800

    Initial commit

---
+++ src/main/java/org/example/labreservationsystem/LabReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.labreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabReservationSystemApplication.class, args);
+    }
+
+}
