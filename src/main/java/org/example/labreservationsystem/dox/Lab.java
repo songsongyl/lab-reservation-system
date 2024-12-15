30203e346fafc726704f7037c7729bb0b8bb7ea2
songsongyl <2444616264@qq.com>
2024-12-15 13:49:02 +0800

    数据库

---
+++ src/main/java/org/example/labreservationsystem/dox/Lab.java
@@ -0,0 +1,9 @@
+package org.example.labreservationsystem.dox;
+
+public class Lab {
+    private String id;
+    private int name;
+    private int state;
+    private  int seatNumber;
+    private String introduction;
+}
